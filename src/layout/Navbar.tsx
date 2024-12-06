0d38310feea08521ec3c5d689f0410ea2b78f10f
boby900 <bobyt2265@gmail.com>
2024-12-06 05:42:43 -0800

    adding hamburger menu

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,25 +1,38 @@
-import { Input } from "@/components/ui/input"
-import { Github } from "lucide-react"
-
-
-
+import { Input } from "@/components/ui/input";
+import { FlameIcon, Github, Menu } from "lucide-react";
 
 function Navbar() {
- 
-
   return (
     <div>
-    <nav className="">
-        <ul>
-            <li>ContentVerse</li>
-            <li><Input type="search" placeholder="Search..." /></li>
-            <li>FAQ</li>
-            <li><Github /></li>
-            <li><a href="#">Documentation.</a></li>
+      <nav>
+        <ul className="flex max-w-full gap-0  sm:gap-4 items-center justify-between lg:border p-0 sm:p-4 m-4 max-h-[80px] lg:border-gray-800">
+          <li className="flex">
+            <span>
+              <FlameIcon />
+            </span>
+            ContentVerse
+          </li>
+          <li>
+            <Input
+              className="w-[200px] lg:w-[600px] h-10"
+              type="search"
+              placeholder="Search..."
+            />
+          </li>
+          <li className="hidden lg:block">FAQ</li>
+          <li className="hidden lg:block">
+           <a href="#"><Github /></a> 
+          </li>
+          <li>
+            <a href="#" className="hidden lg:block">Documentation.</a>
+          </li>
+          <li className="lg:hidden flex-shrink-0 max-w-full">
+            <a href="" className="block"><Menu /></a>
+          </li>
         </ul>
-    </nav>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
