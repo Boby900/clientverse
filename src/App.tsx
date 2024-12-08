056fc8f02823a933de96c0d9a9ee8bc3bd6850db
boby900 <bobyt2265@gmail.com>
2024-12-08 12:55:40 +0530

    added react-router for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
-
-import './App.css'
-import Footer from './layout/Footer'
-import Main from './layout/Main'
-import Navbar from './layout/Navbar'
-import Page2 from './layout/Page2'
+import "./App.css";
+import SignIn from "./auth/SignIn";
+import Footer from "./layout/Footer";
+import Main from "./layout/Main";
+import Navbar from "./layout/Navbar";
+import Page2 from "./layout/Page2";
+import { Routes, Route } from "react-router";
 
 function App() {
- 
-
   return (
     <div>
-       <Navbar />
-       <Main />
-       <Page2 />
-       <Footer />
+     
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+             <Navbar />
+              <Main />
+              <Page2 />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              <SignIn />
+            </>
+          }
+        />
+      </Routes>
+    
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,11 +1,14 @@
 import { ArrowRight } from "lucide-react";
+import { Link } from "react-router";
 
 function Main() {
     return (
       <div className="flex gap-4 min-h-[600px] bg-[url('/cms.png')] bg-cover flex-col justify-center items-center  border border-[#006D75] p-4 m-4">
-        <a href="#">
+        <Link to='/login'>
         <div className="flex border-2  p-4 bg-[#03262b] text-yellow-50 font-thin  border-[#006D75]">Live demo <span className="pl-1"><ArrowRight /></span></div>
-        </a>
+        </Link>
+       
+     
         <a href="">
         <div className="flex border-2  p-4 bg-[#03262b]  text-yellow-50 font-thin border-[#006D75]">Read the documentation <span className="pl-1"><ArrowRight /></span></div>
         </a>
