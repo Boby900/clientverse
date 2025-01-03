46947518ab5c5420ac7204e74f666f16a4dbd132
boby900 <bobyt2265@gmail.com>
2025-01-03 13:49:24 +0530

    added /// directive

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   test: {
-        environment: 'jsdom',
-        setupFiles: ['./tests/setup.ts'],
-        testMatch: ['./tests/**/*.test.tsx'],
-        globals: true
-      },
+    // ... Specify options here.
+  },
+  plugins: [react()],
+ 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
+
