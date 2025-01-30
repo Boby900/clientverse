7a60c92bfaea2c7c040b41300e06e0332a502e11
boby900 <bobyt2265@gmail.com>
2025-01-30 19:23:43 +0530

    ref: remove unused React import

--- src/pages/dashboard/EditCard.tsx
+++ src/pages/dashboard/EditCard.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router"; // Import useParams
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
